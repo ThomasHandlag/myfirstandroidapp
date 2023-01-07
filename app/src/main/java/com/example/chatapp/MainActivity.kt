09acbe914078351b43c178ab6ddd3addcd727a0b
ThomasHandlag <truongthuongvan203@gmail.com>
2023-01-07 11:00:06 +0700

    push

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,33 +1,39 @@
 package com.example.chatapp
 
+import android.icu.text.NumberFormat
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import com.example.chatapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val button: Button = findViewById(R.id.button)
-        val diceRoll = Dice(10)
-        button.setOnClickListener {
-            val textView: TextView = findViewById(R.id.textView)
-            textView.text = diceRoll.plus().toString()
-            val imageView: ImageView = findViewById(R.id.imageView)
-            imageView.setImageResource(R.drawable.ic_launcher_foreground)
+        binding.acBtn.setOnClickListener {
+            binding.rsView.text =
+                getString(R.string.rs_field, NumberFormat.getCurrencyInstance().format(send()))
+        }
+
+        binding.sw1.setOnClickListener {
+            binding.emailField.visibility =
+                if (binding.sw1.isChecked) View.VISIBLE else View.INVISIBLE
         }
     }
-}
 
-class Dice(private val num: Int, var inc: Int = 0) {
-    fun rollDice(): Int {
-        return (1..num).random()
-    }
-
-    fun plus(): Int {
-        return inc++
+    private fun send(): Double {
+        val cont: Double = binding.plainTextInput.text.toString().toDoubleOrNull() ?: 0.0
+        val opt: Double = when (binding.rdGroup.checkedRadioButtonId) {
+            R.id.rd_1 -> 0.5
+            R.id.rd_2 -> 0.2
+            else -> 0.9
+        }
+        return opt * cont
     }
 }
