5dc9ffe99c28997655f5db1245f0264ebd83786f
ThomasHandlag <truongthuongvan203@gmail.com>
2023-03-07 15:53:14 +0700

    io

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.chatapp
 
-import android.icu.text.NumberFormat
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import com.example.chatapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,18 +11,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.acBtn.setOnClickListener {
-            binding.rsView.text =
-                getString(R.string.rs_field, NumberFormat.getCurrencyInstance().format(send()))
+
+        binding.button.setOnClickListener{
+            val text: Double? = binding.plainTextInput.text.toString().toDoubleOrNull();
+            val currency: Double? = when(binding.radioGroup.checkedRadioButtonId){
+                R.id.radioButton ->(text?.div(23000.0))
+                R.id.radioButton2 ->(text?.div(25000.0))
+                R.id.radioButton3 -> (text?.div(40000.0))
+                else -> 0.0
+            }
+            binding.textView.text = currency.toString()
         }
 
-        binding.sw1.setOnClickListener {
-            binding.emailField.visibility =
-                if (binding.sw1.isChecked) View.VISIBLE else View.INVISIBLE
-        }
     }
 
-    private fun send(): Double {
+   /* private fun send(): Double {
         val cont: Double = binding.plainTextInput.text.toString().toDoubleOrNull() ?: 0.0
         val opt: Double = when (binding.rdGroup.checkedRadioButtonId) {
             R.id.rd_1 -> 0.5
@@ -35,5 +33,5 @@ class MainActivity : AppCompatActivity() {
             else -> 0.9
         }
         return opt * cont
-    }
+    }*/
 }
